c17087a3520771e2e7d4cef0ad035661b91b70f2
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-16 20:03:34 +0300

    relations built

--- app/Models/Hit.php
+++ app/Models/Hit.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use App\Models\Query;
 use Illuminate\Database\Eloquent\Model;
 
 class Hit extends Model
 {
     protected $guarded = [];
+
+    public function query()
+    {
+        return $this->belongsTo(Query::class);
+    }
 }
--- app/Models/Query.php
+++ app/Models/Query.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Models\User;
+use App\Models\{User, Hit};
 use Illuminate\Database\Eloquent\Model;
 
 class Query extends Model
@@ -12,5 +12,10 @@ class Query extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function hits()
+    {
+        return $this->hasMany(Hit::class);
     }
 }
